fe92623587e40ff9163afe83c82734266a3ad3aa
jmchavesfilho <maurinhoshaq@hotmail.com>
2024-11-22 16:58:57 -0300

    atualização das models de serviço 21/11/2024 as 16:58

--- servicos/models.py
+++ servicos/models.py
@@ -1,8 +1,21 @@
 from django.db import models
 from clientes.models import Cliente
 
+
+class CategoriaManutencao(models.Model):
+    titulo = models.CharField(max_length=3)
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def _str_(self) -> str:
+        return self.titulo
+    
 class Servico(models.Model):
     titulo = models.CharField(max_length=30)
-    cliente = models.ForeignKey()
+    cliente = models.ForeignKey(Cliente, on_delete=models.SET_NULL, null=True)
+
+    data_inicio = models.DateField(null=True)
+    data_entrega = models.DateField(null=True)
+    finalizado = models.BooleanField(default=False)
+    protocole = models.CharField(max_length=32, null=True, blank=True)
 
     #aula 5, 08:38 minuto 

113ba7e000138dee25bb117c3b4d328b4e652c2b
José Mauro Chaves Filho <maurinhoshaq@hotmail.com>
2024-12-04 22:42:40 -0300

    finalização da aula 5

--- servicos/models.py
+++ servicos/models.py
@@ -1,6 +1,10 @@
+from email.policy import default
+from secrets import token_hex
 from django.db import models
 from clientes.models import Cliente
 from .choices import ChoicesCategoriaManutencao
+from datetime import datetime
+
 
 
 class CategoriaManutencao(models.Model):
@@ -17,6 +21,21 @@ class Servico(models.Model):
     data_inicio = models.DateField(null=True)
     data_entrega = models.DateField(null=True)
     finalizado = models.BooleanField(default=False)
-    protocole = models.CharField(max_length=32, null=True, blank=True)
+    protocolo = models.CharField(max_length=52, null=True, blank=True)
 
-    #aula 5, 08:38 minuto 
+    def _str_(self) -> str:
+        return self.titulo
+    
+    def save(self, *args, **kwargs):
+        if not self.protocolo:
+            self.protocolo = datetime.now().strftime("%d/%m/%Y-%H:%M:%S-") + token_hex(16)
+        super(Servico, self).save(*args, **kwargs)
+
+    def preco_total(self):
+        preco_total = float(0)        
+        for categoria in self.categoria_manutencao.all():
+            preco_total += float(categoria.preco)
+
+        return preco_total    
+
+

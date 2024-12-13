198934b5235aaaea3e8c7c8ba477d45eb59027f1
jmchavesfilho <maurinhoshaq@hotmail.com>
2024-12-13 16:42:06 -0300

    aula 06 20min 13/12/2024

--- servicos/forms.py
+++ servicos/forms.py
@@ -7,6 +7,9 @@ class FormServico(ModelForm):
         exclude = ['finalizado', 'protocolo']
 
     def _init_(self, *args, **kwargs):
+        super()._init_(*args, **kwargs)     
+        print(self.fields['titulo'].widget.att)
 
 
-        #aula 06  parada 17min
+
+        #aula 06  parada 20min
